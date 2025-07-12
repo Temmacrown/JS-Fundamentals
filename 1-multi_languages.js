5ee564e3ab8cc69d6ba8c13712222c00f36b972d
Temmacrown <johnadekunle41@.com>
2025-07-12 21:26:09 +0100

    Add script that prints 3 programming language lines

---
+++ 1-multi_languages.js
@@ -0,0 +1,6 @@
+echo 'const line1 = "C is fun";' > 1-multi_languages.js
+echo 'const line2 = "Python is cool";' >> 1-multi_languages.js
+echo 'const line3 = "JavaScript is amazing";' >> 1-multi_languages.js
+echo 'console.log(line1);' >> 1-multi_languages.js
+echo 'console.log(line2);' >> 1-multi_languages.js
+echo 'console.log(line3);' >> 1-multi_languages.js
