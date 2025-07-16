cec650b384c1b6ad45c831e1c64a14e319ab030a
Temmacrown <johnadekunle41@.com>
2025-07-16 13:24:08 +0100

    Add script that prints 3 lines about C, Python, and JavaScript

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,5 +1,7 @@
-guillaume@ubuntu:~/0x12$ node 1-multi_languages.js 
-C is fun
-Python is cool
-JavaScript is amazing
-guillaume@ubuntu:~/0x12$ 
+const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "JavaScript is amazing";
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
